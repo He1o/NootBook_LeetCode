ce24e672b43fa490ed31c0955fe733844c3fe9c0
MidSummerSiesta <1210568288@qq.com>
2021-08-22 15:59:47 +0800

    0822

--- 96.Unique Binary search Trees.py
+++ 96.Unique Binary search Trees.py
@@ -9,3 +9,13 @@ Example 2:
 Input: n = 1
 Output: 1
 '''
+
+class Solution:
+    def numTrees(self, n):
+        dp = [0] * (n + 1)
+        dp[0] = dp[1] = 1
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                dp[i] += dp[j - 1] * dp[i - j]
+        
+        return dp[n]
